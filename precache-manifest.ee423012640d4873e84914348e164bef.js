f309f63cb94a75b6f1c182a4f43f800db1ea88b7
matthaussousa <mtthsousa@gmail.com>
2018-12-11 17:39:12 -0200

    Add files via upload

---
+++ precache-manifest.ee423012640d4873e84914348e164bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a927065010563de69ff8e97f102f38bd",
+    "url": "/static/media/add_photo.a9270650.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d247b072b61ced5f67d",
+    "url": "/static/js/main.4d247b07.chunk.js"
+  },
+  {
+    "revision": "b2fa704d401906c7ea5a",
+    "url": "/static/js/1.b2fa704d.chunk.js"
+  },
+  {
+    "revision": "4d247b072b61ced5f67d",
+    "url": "/static/css/main.8233daf3.chunk.css"
+  },
+  {
+    "revision": "b3bb48c008e7dbf518dda856ffdca4f4",
+    "url": "/index.html"
+  }
+];
